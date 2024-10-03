5db13b21be89a52208379cf417b349cf823aad4f
Phowk <estheban1023@gmail.com>
2024-10-02 20:02:22 -0500

    Agregar función para mostrar notas

--- notas.py
+++ notas.py
@@ -1,16 +1,29 @@
 def agregar_nota(nota):
-with open("notas.txt", "a") as archivo:
-   archivo.write(nota + "\n")
+   with open("notas.txt", "a") as archivo:
+      archivo.write(nota + "\n")
+
 print("Nota agregada.")
+
 def mostrar_notas():
-try:
-   with open("notas.txt", "r") as archivo:
-       notas = archivo.readlines()
-   print("Notas:")
-   for nota in notas:
-       print(nota.strip())
-except FileNotFoundError:
-   print("No hay notas guardadas.")
+   try:
+      with open("notas.txt", "r") as archivo:
+          notas = archivo.readlines()
+      print("Notas:")
+      for nota in notas:
+          print(nota.strip())
+   except FileNotFoundError:
+      print("No hay notas guardadas.")
+
+def mostrar_notas():
+  try:
+      with open("notas.txt", "r") as archivo:
+          notas = archivo.readlines()
+      print("Notas:")
+      for nota in notas:
+          print(nota.strip())
+  except FileNotFoundError:
+      print("No hay notas guardadas.")
+
 if __name__ == "__main__":
-agregar_nota("Esta es una nota de prueba.")
-mostrar_notas()
+   agregar_nota("Esta es una nota de prueba.")
+   mostrar_notas()
